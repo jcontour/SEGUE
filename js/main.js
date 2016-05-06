581afe50cf339085eda7915879d4ee5c9d8d1110
jcontour <contj186@newschool.edu>
2016-05-06 18:17:22 -0400

    kinda working, getting infinite loop error

--- js/main.js
+++ js/main.js
@@ -4,34 +4,14 @@ app.main = (function(){
 
 	console.log('Loading app.');
 
-	var getInput = function(){
-		loadNYT($('#search-box').val());
-	};
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// 		SETUP FUNCTIONS
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-	var attachEvents = function(){
-
-		// console.log('Attaching events.');
-
-		$('#search-button').on('click', getInput);
-		
-		$('#search-box').keypress(function(e){
-			if (e.which == 13){
-				getInput();
-			}
-		});
-
-		$('.article-item').off('click').on('click', function(){
-			var searchCN = this.id
-			console.log("searchCN : ", searchCN);
-			loadConceptNet(searchCN);
-		})
-
-		$('.edge-item').off('click').on('click', function(){
-			var searchNYT = this.id;
-			console.log("searchNYT : ", searchNYT);
-			loadNYT(searchNYT);
-		})
-	};
+	var graph;
+	var dataId = 1;
+	var clickedId = 1;
+	var graphData;
 
 	var render = function(whichtemplate, container, method, data){
 		var htmlTemplate = $('#tpl-' + whichtemplate).html();
@@ -51,75 +31,262 @@ app.main = (function(){
         attachEvents();
 	};	
 
+	var getInput = function(){
+
+		graphData = {'name': $('#search-box').val(), 'searchTerm': $('#search-box').val(), 'type': 'cn', 'id': dataId };
+		dataId ++;
+		
+		graph = new Graph();
+		graph.setup(graphData);
+
+		console.log("graph data ", graphData)
+
+		loadNYT($('#search-box').val());
+	};
+
+	var attachEvents = function(){
+		$('#search-button').on('click', getInput);
+		
+		$('#search-box').keypress(function(e){
+			if (e.which == 13){
+				getInput();
+			}
+		});
+
+		$('.article-item').off('click').on('click', function(){
+			var searchCN = this.id
+			console.log("searchCN : ", searchCN);
+			loadConceptNet(searchCN);
+		})
+
+		$('.edge-item').off('click').on('click', function(){
+			var searchNYT = this.id;
+			console.log("searchNYT : ", searchNYT);
+			loadNYT(searchNYT);
+		})
+
+		$('.display').off('click').on('click', function(){
+			
+			var search = $(this).attr("data-search");
+
+			clickedId = $(this).attr("data-id")
+			
+			console.log("clicked ", $(this).attr('data-search'), "from ", $(this).attr("data-type"), "and be a child of ", clickedId)
+			
+			if ($(this).attr('data-type') == "cn") {
+				console.log("search nyt : ", search);
+				loadNYT(search);
+			} else if ($(this).attr('data-type') == "nyt") {
+				console.log("search cn : ", search);
+				loadConceptNet(search);
+			}
+		});	
+			
+		$('circle').off('click').on('click', function(){
+			
+			var search = this.id;
+
+			clickedId = $(this).attr("data-tag");
+			
+			console.log("clicked ", this.id, "clicked Id ", clickedId)
+			
+			if ($(this).attr("data-which") == 'cn') {
+				console.log("search nyt : ", search);
+				loadNYT(search);
+			} else if ($(this).attr("data-which") == 'nyt') {
+				console.log("search cn : ", search);
+				loadConceptNet(search);
+			}
+
+		});
+	};
+
+	var pushData = function(array, which){
+		console.log("pushing results from ", which)
+
+		var children = [];
+
+		for (var i = 0; i < array.length; i++){
+			var x = {'name': array[i].name, 'searchTerm': array[i].id, 'type': which, 'id': dataId , url: array[i].url}
+			children.push(x);
+			dataId ++;
+		}
+
+		if (which == "nyt"){
+			console.log("rendering results from nyt")
+			render('articles', 'article-container', 'replace', 
+					{ data: {
+							results: array
+						}
+					});
+		} else if (which == "cn"){
+			console.log("rendering results from nyt")
+			render('edges', 'edge-container', 'replace', 
+					{ data: {
+							results: array
+						}
+					});
+		}
+
+		console.log("graph data ", graphData, "id to find ", clickedId, "child data ", children);
+		var updatedData = insertById(graphData, clickedId, children);
+		console.log("update graph data ", graphData)
+		graph.update(updatedData);
+	
+		// render('display', 'display-container', 'replace', 
+		// 	{ data: {
+		// 			results: graphData
+		// 		}
+		// 	});
+
+		attachEvents();
+	}
+
+	var insertById = function(data, id, children) {
+	    //Early return
+	    if( data.id === id ){
+	      data['children'] = children;
+	      return data;
+	    }
+	    var result, p; 
+	    
+	    for (p in data) {
+	        if( data.hasOwnProperty(p) && typeof data[p] === 'object' ) {
+	            result = insertById(data[p], id, children);
+	            if(result){
+	                return result;
+	            }
+	        }
+	    }
+	    return result;
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// 		CHART SETUP
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+	var Graph = function(){
+
+		var obj = {};
+
+		var margin = { top: 20, right: 20, bottom: 20, left: 20 };
+		var width = 900 - margin.left - margin.right;
+		var height = 600 - margin.top - margin.bottom;                
+		var svg, graph;
+		var cluster;
+		var diagonal;
+
+		obj.setup = function(dataset){
+
+			cluster = d3.layout.cluster()
+    			.size([height, width - 160]);
+
+    		diagonal = d3.svg.diagonal()
+    			.projection(function(d) { return [d.y, d.x]; });
+
+    		svg = d3.select("#graph")
+	    		.append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+			
+			graph = svg.append("g")
+			.attr("transform", 'translate('+ margin.left + ', ' + margin.top + ')')
+
+			obj.update(dataset);
+		}
+
+		obj.update = function(dataset){
+			var graphingData = dataset
+			// console.log("graph data ", JSON.stringify(dataset))
+
+			var nodes = cluster.nodes(graphingData);
+      		var links = cluster.links(nodes);
+
+			var link = svg.selectAll(".link")
+				.data(links)
+
+			link.enter().append("path")
+				.attr("class", "link")
+				.attr("d", diagonal)
+				.attr("transform", function(d) { return "translate( 100, 0 )"; })
+				;
+
+			var node = svg.selectAll(".node")
+				.data(nodes)
+
+			node.enter().append("g")
+				.attr("class", "node")
+				.attr("transform", function(d) { return "translate(" + (d.y + 100) + "," + d.x + ")"; })
+
+			node.append("circle")
+				.attr("r", 50)
+				.attr('data-which', function(d){ return d.type; })
+				.attr('class', function(d){ return d.type; })
+				.attr('id', function(d){ return d.searchTerm })
+				.attr('data-tag', function(d){ return d.id })
+
+			node.append("text")
+				.attr("dx", function(d) { return d.children ? -5 : 5; })
+				.attr("dy", 3)
+				.style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+				// .style("font-size", "px")
+				.style("fill", "white")
+				.text(function(d) { return d.searchTerm; });
+
+			d3.select(self.frameElement).style("height", height + "px");
+			attachEvents();
+
+		}
+
+		return obj;
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// 		SEARCH CN THEN DOUBLE CHECK NYT
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 	var loadConceptNet = function(searchTerm){
+		console.log("searching concept net for ", searchTerm);
 
 		var searchConceptNet = 'http://conceptnet5.media.mit.edu/data/5.4/c/en/' + searchTerm + '?filter=/c/en';
 		
 		$.getJSON(searchConceptNet, function(json){
-			console.log("Data received from concept net : ", json.edges.length);
-			
+			// console.log("Data received from concept net : ", json.edges.length);
 			var resultsCN = json.edges;
-			
 			var resultFilter = [];
 
 			for (var i = 0; i < resultsCN.length; i++){
 				
-				//filter for edge end, single word results. 
-				console.log("i ", i, " edge start : ", resultsCN[i].surfaceStart, ", edge end : ", resultsCN[i].surfaceEnd);
-				console.log("result ", resultsCN[i]);
-				// if (resultsCN[i] !== undefined && resultsCN[i].start == "/c/en/" + searchTerm && resultsCN[i].end.indexOf("_") == -1){
+				// filter for results in english
 				if (resultsCN[i] !== undefined && (resultsCN[i].dataset == "/d/wiktionary/en/en" 
 													|| resultsCN[i].dataset == "/d/conceptnet/4/en" 
 													|| resultsCN[i].dataset == "/d/umbel" 
 													|| resultsCN[i].dataset == "/d/dbpedia/en")){
-					console.log(resultsCN[i].surfaceStart.toLowerCase(), searchTerm, resultsCN[i].surfaceEnd.toLowerCase())
+					// console.log(resultsCN[i].surfaceStart.toLowerCase(), searchTerm, resultsCN[i].surfaceEnd.toLowerCase())
 
 					if (resultsCN[i].surfaceStart.toLowerCase() == searchTerm){
+						resultsCN[i].name = resultsCN[i].surfaceEnd.toLowerCase()
 						resultsCN[i].id = resultsCN[i].surfaceEnd.toLowerCase()
 					} else {
+						resultsCN[i].name = resultsCN[i].surfaceStart.toLowerCase()
 						resultsCN[i].id = resultsCN[i].surfaceStart.toLowerCase()
 					}
 					
-					console.log("pushing ", resultsCN[i].id)
 					resultFilter.push(resultsCN[i]);
-
-					// if (i == (resultsCN.length - 1)) {
-					// 	console.log("done with resultsCN");
-					// 	break;
-					// }
 				}
-				console.log("next CN result");
 			}
 
-			console.log("resultFilter ", resultFilter);
-
+			console.log("resultFilter ",  resultFilter);
 			doubleCheckNYT(0, resultFilter, [])
-
-			// ------------------------------------------------------------------------------------------------
-			// THIS WILL BE UPDATING THE GRAPH WITH THE CN DATA INSTEAD OF RENDERING
-			// ------------------------------------------------------------------------------------------------
-
-			// $('#edge-container').empty();
-			// for (var i = 0; i < resultFilter.length; i ++ ){
-			// 	render('edges', 'edge-container', 'append',
-			// 			{data: {
-			// 					results: resultFilter[i],
-			// 					term: searchTerm
-			// 				}
-			// 			});
-			// }
-
-			// attachEvents();
 
 		});
 	}
 
 	var doubleCheckNYT = function(num,data,array){		// this checks if the id gets results from nyt and removes if not
-		if (num < data.length){
-			// console.log("num ", num, " of ", data.length, " results.") 
-			// console.log("id ", data[num].id, " array ", array);
-
-			if ($.inArray(data[num].id, array) == -1){
+		console.log("double checking concept net results against nyt")
+		// console.log(array.length, array);
+		if (num < data.length && array.length < 5){
+			if ($.inArray(data[num].id, array) == -1){		// if id is not in array already, search NYT and check for results
 				// console.log("id not in array");
 
 				var searchNYT = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ data[num].id +'&page=0&sort=newest&api-key=b513fe756c28fddfee540182bb4d37a6:1:59936560'
@@ -127,31 +294,34 @@ app.main = (function(){
 				$.getJSON(searchNYT, function(json){
 					// console.log(data[num].id, " gets ", json.response.docs.length , " results from NYT")
 
-					if (json.response.docs.length > 0 ){
-						
-						array.push(data[num].id)
-
-						render('edges', 'edge-container', 'append',
-							{ data: {
-									results: data[num]
-								}
-							});
+					if (json.response.docs.length > 0 ){		// if there are results, push them into the array. 
+						array.push(data[num])
 					} 
 					
 					num++;
 					doubleCheckNYT(num, data, array);
 				});
-			} else {
-			// console.log("skipping duplicate")
-			num++;
-			doubleCheckNYT(num, data, array);
+
+			} else {	// skip duplicates
+				num++;
+				doubleCheckNYT(num, data, array);
 			}
+		} else {
+			console.log("done", array)
+			pushData(array, "cn")
 		}
+
 	}
+
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	// 		SEARCH NYT THEN DOUBLE CHECK CN
+	// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 	var resultsNYT = []
 
 	var loadNYT = function(searchTerm){
+		console.log("searching nyt for ", searchTerm);
+
 		var searchNYT = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+searchTerm+'&page=0&sort=newest&api-key=b513fe756c28fddfee540182bb4d37a6:1:59936560'
 		
 		$.getJSON(searchNYT, function(json){
@@ -164,6 +334,9 @@ app.main = (function(){
 			
 			//setting id for searching concept net
 			for (var i = 0; i < resultsNYT.length; i ++ ){
+				resultsNYT[i].name = resultsNYT[i].headline.main
+				resultsNYT[i].url = resultsNYT[i].web_url
+
 				if (resultsNYT[i].keywords.length > 0 ){
 					var keywordSplit = resultsNYT[i].keywords[0].value.split(" ");
 					
@@ -195,15 +368,13 @@ app.main = (function(){
 	};
 
 	var doubleCheckCN = function(num,data){		// this checks if the id gets results from concept net and removes if not
-		// console.log("num ", num, " data ", data);
 
 		if (num == data.length){
-			render('articles', 'article-container', 'replace', 
-					{data: {
-							results: data
-						}
-					});
+
+			pushData(data, "nyt");
+
 		} else {
+
 			var searchConceptNet = 'http://conceptnet5.media.mit.edu/data/5.4/c/en/' + data[num].id + '?filter=/c/en';
 		
 			$.getJSON(searchConceptNet, function(json){
